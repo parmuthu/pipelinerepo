f9751288b2f658e1818e1f8c428e3cce078f31f1
parmuthu <parmuthu2002@gmail.com>
2025-01-30 15:20:57 +0530

    main class modified

--- src/main/java/com/awsapp/ecsdemo/EcsdemoApplication.java
+++ src/main/java/com/awsapp/ecsdemo/EcsdemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcsdemoApplication {
 
 	public static void main(String[] args) {
-		System.out.print("Main applictation changed");
+		System.out.print("Main applictation modified");
 		SpringApplication.run(EcsdemoApplication.class, args);
 		
 	}
